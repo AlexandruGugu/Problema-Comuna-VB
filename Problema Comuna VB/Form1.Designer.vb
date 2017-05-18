f63bf993769609ef33c94483540ef05c98d150c8
Alex (Ecko88) <g.alexandru33@yahoo.com>
2017-05-18 11:09:17 +0300

    Add project files.

---
+++ Problema Comuna VB/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(514, 253)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 39)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Selectare Fisier"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(835, 137)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(835, 199)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Salvare"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1201, 640)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
---
+++ Problema Comuna VB/Form1.vb
@@ -0,0 +1,89 @@
+﻿Public Class Form1
+    Public text As String
+    Public dicCaractere As Dictionary(Of String, Integer) = New Dictionary(Of String, Integer)
+    Public dicCuvinte As Dictionary(Of String, Integer) = New Dictionary(Of String, Integer)
+    Public dicFraze As Dictionary(Of String, Integer) = New Dictionary(Of String, Integer)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (OpenFileDialog1.ShowDialog() = DialogResult.OK) Then
+            Dim sr As New System.IO.StreamReader(OpenFileDialog1.FileName)
+            text = sr.ReadToEnd
+        End If
+    End Sub
+
+    Public Sub Caractere()
+        For Each caracter As Char In text
+            If dicCaractere.ContainsKey(caracter.ToString()) Then
+                dicCaractere.Item(caracter.ToString()) += 1
+            Else
+                dicCaractere.Add(caracter.ToString(), 1)
+            End If
+        Next
+    End Sub
+    Public Sub Cuvinte()
+        Dim cuvinte() As String = text.Split((" ,.!?()[]{}=+<>/" + vbNewLine + vbCr + vbLf).ToCharArray, StringSplitOptions.RemoveEmptyEntries)
+        For Each cuvant As String In cuvinte
+            If IsNumeric(cuvant) Then
+                Continue For
+            End If
+            If dicCuvinte.ContainsKey(cuvant) Then
+                dicCuvinte.Item(cuvant) += 1
+            Else
+                dicCuvinte.Add(cuvant, 1)
+            End If
+        Next
+    End Sub
+    Public Sub Fraze()
+        Dim i As Integer = text.IndexOf(vbNewLine, 0)
+        Dim j As Integer = 0
+        Dim substr As String
+        While i <> -1
+            substr = text.Substring(j, i - j)
+            If dicFraze.ContainsKey(substr) Then
+                dicFraze.Item(substr) += 1
+            Else
+                dicFraze.Add(substr, 1)
+            End If
+            j = i + 1
+            i = text.IndexOf(vbNewLine, j)
+        End While
+    End Sub
+    Public Sub Fraze2()
+        Dim fraze() As String = text.Split((vbNewLine + vbCr + vbLf).ToCharArray, StringSplitOptions.RemoveEmptyEntries)
+        For Each fraza As String In fraze
+            If IsNumeric(fraza) Then
+                Continue For
+            End If
+            If dicFraze.ContainsKey(fraza) Then
+                dicFraze.Item(fraza) += 1
+            Else
+                dicFraze.Add(fraza, 1)
+            End If
+        Next
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Caractere()
+        Cuvinte()
+        Fraze2()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+            Dim sw As New System.IO.StreamWriter(SaveFileDialog1.FileName)
+            sw.WriteLine("----- FRAZE -----")
+            WriteToFile(sw, dicFraze)
+            sw.WriteLine("----- CUVINTE -----")
+            WriteToFile(sw, dicCuvinte)
+            sw.WriteLine("----- CARACTERE -----")
+            WriteToFile(sw, dicCaractere)
+            sw.Close()
+        End If
+    End Sub
+    Public Sub WriteToFile(file As System.IO.StreamWriter, dic As Dictionary(Of String, Integer))
+        Dim keys As List(Of String) = dic.Keys.ToList
+        keys.Sort()
+        For Each key As String In keys
+            file.WriteLine(key + ": " + dic(key).ToString())
+        Next
+    End Sub
+
+End Class
